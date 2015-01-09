38e25df28145e843130aa717a6cb6f79119934c1
andars <afoote97@gmail.com>
2015-01-08 20:54:44 -0800

    Initial working prototype of vm and assembler
    Next up: labels

--- assembler/assembler.c
+++ assembler/assembler.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
+
+#include "../codes.h"
 
 char* readFile(char* name) {
     FILE *input = fopen(name, "r");
@@ -23,11 +26,74 @@ int main(int argc, char** argv) {
         return 1;
     }
     char* source = readFile(argv[1]);    
+    int source_length = strlen(source);
+
     char* token = strtok(source, " \n");
-    while (token) {
-        printf("%s\n", token);
+
+//still trying to decide if I want to go for 2 pass or 1 pass
+/*
+    char *transformed_source = malloc(sizeof(char*) * source_length);
+    int transformed_length = 0;
+    //first pass for labels
+    
+    int length = strlen(token);
+    for (int i = length; i<source_length; i+= length) {
+
+        length = strlen(token);
+        if (token[length-1] == ':') {
+        } else {
+            strcpy(transformed_source+transformed_length, token);
+            strcpy(transformed_source+transformed_length+length, " ");
+            transformed_length += length + 1;
+        }
         token = strtok(NULL, " \n");
+        if (token == NULL) break;
     }
+    transformed_source[transformed_length] = '\0';
+
+    fprintf(stderr, "%s", transformed_source);
+*/
+    char output;
+    int position = 0;
+    while (token) {
+        if(strcasecmp(token, "push") == 0) {
+            printf("%c", BC_PUSH);
+            token = strtok(NULL, " \n");
+            uint8_t value = strtol(token, NULL, 10);
+            printf("%c", value);
+        } else if (strcasecmp(token, "pop") == 0) {
+            output = BC_POP;
+            printf("%c", output);
+        } else if (strcasecmp(token, "add") == 0) {
+            output = BC_ADD;
+            printf("%c", output);
+        } else if (strcasecmp(token, "neg") == 0) {
+            output = BC_NEG;
+            printf("%c", output);
+        } else if (strcasecmp(token, "jump") == 0) {
+            printf("%c", BC_JUMP);
+            token = strtok(NULL, " \n");
+            uint8_t value = strtol(token, NULL, 10);
+            printf("%c", value);
+        } else if (strcasecmp(token, "cmp") == 0) {
+            output = BC_CMP;
+            printf("%c", output);
+        } else if (strcasecmp(token, "rjmp") == 0) {
+            printf("%c", BC_RJUMP);
+            token = strtok(NULL, " \n");
+            uint8_t value = strtol(token, NULL, 10);
+            printf("%c", value);
+        } else {
+            char last = token[strlen(token)-1];
+            fprintf(stderr, "%c", last);
+            if (last == ':') { //label
+
+            }
+        }
+
+
+        token = strtok(NULL, " \n");
+    } 
 
     free(source);    
 }
--- vm.c
+++ vm.c
@@ -11,6 +11,7 @@ int stack_pointer = 0;
 int base_pointer = 0;
 int stack_top = 0;
 
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         fprintf(stderr, "Usage: vm filename\n");
@@ -20,20 +21,49 @@ int main(int argc, char** argv) {
     uint8_t bytecode = 0;
 
     while(fread(&bytecode, 1, 1, input)) {
-//        printf("%d", bytecode);
+        uint8_t a;
+        uint8_t b;
+        
+        printf("read bytecode: ");
         switch (bytecode) {
-        case BC_PUSH: {
-            uint8_t a = 0;
+        case BC_PUSH: 
+            printf("BC_PUSH\n");
             fread(&a, 1, 1, input);
             stack[++stack_top] = a;
             break;
-        }
         case BC_POP:
+            printf("BC_POP\n");
             printf("%d\n", stack[stack_top]);
             stack[stack_top--] = 0;
+            break;
+        case BC_ADD:
+            printf("BC_ADD\n");
+            a = stack[stack_top--];
+            b = stack[stack_top--];
+            stack[++stack_top] = a+b;
+            break;
+        case BC_NEG:
+            printf("BC_NEG\n");
+            stack[stack_top] = -stack[stack_top];
+            break;
+        case BC_JUMP:
+            printf("BC_JUMP\n");
+            fread(&a, 1, 1, input);
+            fseek(input, a, SEEK_SET);
+            break;
+        case BC_RJUMP:
+            printf("BC_JUMP\n");
+            fread(&a, 1, 1, input);
+            fseek(input, a, SEEK_CUR);
+            break;
         default:
+            printf("unrecognized\n");
             break;                
         } 
+        if (stack_top >= STACK_SIZE) {
+            printf("ERROR: stack overflow\n");
+            return 1;
+        }
     }
     return 0; 
 }
